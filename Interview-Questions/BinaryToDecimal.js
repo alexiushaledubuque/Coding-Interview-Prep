368ea3eab5d6fdf94cae968079879e420dcce43e
Alexius Hale-Dubuque <alexiushaledubuque@yahoo.com>
2016-12-17 22:05:55 -0500

    Interview-Questions/BinaryToDecimal.js

---
+++ Interview-Questions/BinaryToDecimal.js
@@ -0,0 +1,16 @@
+function binaryToDecimal (n) {
+  if (n === 0) return 0
+  if (n === 1) return 1
+
+  let result = 0
+
+  const binaryArray = n.split('').reverse()
+
+  for (let i = 0; i < binaryArray.length; i++){
+    if (Number(binaryArray[i]) === 1){
+      result += Math.pow(2, i)
+    }
+  }
+
+  return result
+}
