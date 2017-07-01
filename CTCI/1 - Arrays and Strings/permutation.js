b73caa86d45b74a7ceb804f597ffe5ff4cdb0d3b
Alexius Hale-Dubuque <alexiushaledubuque@yahoo.com>
2017-07-01 09:10:16 -0400

    String & Number Permutations

---
+++ CTCI/1 - Arrays and Strings/permutation.js
@@ -0,0 +1,39 @@
+// String Permutation
+const permutation = (inputArr) => {
+  let result = [];
+  inputArr = inputArr.split('')
+  const permute = (arr, m = []) => {
+    if (arr.length === 0) {
+      result.push(m.join(''))
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        let curr = arr.slice();
+        let next = curr.splice(i, 1);
+        permute(curr.slice(), m.concat(next))
+    }
+  }
+}
+permute(inputArr)
+
+return result;
+}
+permutation('cat');
+
+// Integer Permutation
+const permutation = (inputArr) => {
+  let result = [];
+  const permute = (arr, m = []) => {
+    if (arr.length === 0) {
+      result.push(m)
+    } else {
+      for (let i = 0; i < arr.length; i++) {
+        let curr = arr.slice();
+        let next = curr.splice(i, 1);
+        permute(curr.slice(), m.concat(next))
+     }
+   }
+ }
+ permute(inputArr)
+ return result;
+}
+permutation([1,2,3]);
